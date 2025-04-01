f2693fe99b7b7326d9a4b2526db5441b61cc1b81
MaxBurks <mburks3@cocc.edu>
2025-04-01 09:10:19 -0700

    Add files via upload

---
+++ Burks.py
@@ -0,0 +1,17 @@
+#Written by Max
+def rect_area(side):
+    '''
+
+    This function calculates the area of a rectangle
+    
+    Parameters
+    ----------
+    int: length
+    int: width
+    
+    Return
+    ------
+    length*width
+    
+    '''
+    return(side**2)
