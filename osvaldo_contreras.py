d53d4c82b6e788ef33e388d9146f026a8aec2c5e
MaxBurks <mburks3@cocc.edu>
2025-04-06 14:12:20 -0700

    Update osvaldo_contreras.py

--- osvaldo_contreras.py
+++ osvaldo_contreras.py
@@ -2,7 +2,7 @@
 #Function 2
 #Returns Surface Area of Rectangular Solid
 #written by Osvaldo Contreras
-def rect_surface_area():
+def rect_surface_area(square):
     """
     Calculates the surface area of a cube by multiplying square by 6
 
